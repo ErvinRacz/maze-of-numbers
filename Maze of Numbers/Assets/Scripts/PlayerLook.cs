8525fb34918b2aba9509ee45e7e84e7693aea42e
Ervin Racz <rcz.ervin@gmail.com>
2019-03-15 21:00:27 +0200

    Implement simple movement control

--- Maze of Numbers/Assets/Scripts/PlayerLook.cs
+++ Maze of Numbers/Assets/Scripts/PlayerLook.cs
@@ -4,17 +4,69 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    [SerializeField] private string mouseXInputName;
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform playerBodyTransform;
+
+    // xAxisClamp is needed to not flip over with the mouse
+    private float xAxisClamp;
+
+    // Awake is called when the script instance is loaded
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CameraRotation();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            // Stops the camera rotation when the player looks up
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            // Stops the camera rotation when the player looks down
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        // To look left and right:
+        playerBodyTransform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
     }
 }
