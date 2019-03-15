59c2b8a22623e581dd5f1fa0558ff168cbf75915
Ervin Racz <rcz.ervin@gmail.com>
2019-03-15 22:28:30 +0200

    Modify player behavior to meet gravity requirements

--- Maze of Numbers/Assets/Scripts/PlayerMove.cs
+++ Maze of Numbers/Assets/Scripts/PlayerMove.cs
@@ -7,24 +7,42 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private string horizontalInputName = "Horizontal";
     [SerializeField] private string verticalInputName = "Vertical";
 
+    private CapsuleCollider playerCollider;
+
+    // Gravity related stuffs
+    [SerializeField] private float gravity = 50; // gravity acceleration
+    [SerializeField] private float changeGravitySpeed = 10;
+    private Rigidbody rigidBody; // rigid body of the player
+    private Vector3 normal;
+    private Vector3 targetSurfaceNormal;
+    private Ray ray;
+    private RaycastHit hit;
+    private float distGround; //distance between player position to ground
+    public float deltaGrounded = 0.2f; //player is grounded upto this distance
+
+    // Moving speed related stuffs
     [SerializeField] private float walkSpeed = 6.0f;
     [SerializeField] private float runSpeed = 9.0f;
     [SerializeField] private float runBuildUpSpeed = 3.0f;
-    private float movementSpeed;
     [SerializeField] private KeyCode runKey = KeyCode.LeftShift;
+    private float movementSpeed;
 
-    private CharacterController characterController;
-
+    // Jump related variables
     [SerializeField] private AnimationCurve jumpFallOff;
     [SerializeField] private float jumpMultiplier = 9.0f;
     [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
+    private CharacterController characterController;
 
     private bool isJumping;
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        rigidBody = GetComponent<Rigidbody>();
+        playerCollider = characterController.GetComponent<CapsuleCollider>(); // TODO: this is not working
+        normal = transform.up;
+        distGround = playerCollider.bounds.extents.y - playerCollider.center.y;
     }
 
     // Start is called before the first frame update
@@ -39,6 +57,12 @@ public class PlayerMove : MonoBehaviour
         PlayerMovement();
     }
 
+    // MonoBehaviour.FixedUpdate has the frequency of the physics system
+    private void FixedUpdate()
+    {
+        rigidBody.AddForce(-gravity * rigidBody.mass * normal);
+    }
+
     private void PlayerMovement()
     {
         float vertInput = Input.GetAxis(verticalInputName);
@@ -47,9 +71,28 @@ public class PlayerMove : MonoBehaviour
         Vector3 forwardMovement = transform.forward * vertInput;
         Vector3 rightMovement = transform.right * horizInput;
 
-        characterController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
         SetMovementSpeed();
+        // Handle Jump
         JumpInput();
+
+        // Move on the plane
+        characterController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+
+        //update surface normal and isGrounded
+        ray = new Ray(transform.position, -normal); //cast ray downwards
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetSurfaceNormal = hit.normal;
+        }
+        else
+        {
+            targetSurfaceNormal = Vector3.up;
+        }
+
+        normal = Vector3.Lerp(normal, targetSurfaceNormal, changeGravitySpeed * Time.deltaTime);
+        Vector3 myForward = Vector3.Cross(transform.right, normal);
+        Quaternion targetRot = Quaternion.LookRotation(myForward, normal);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, changeGravitySpeed * Time.deltaTime);
     }
 
     private void SetMovementSpeed()
